2765089463b145a8a4f6225620767360b55e6052
Oleg Magarill <oleg.magarill@gmail.com>
2021-12-20 11:17:10 +0200

    FirstCommit

---
+++ JS/MyWebPage/MyWebPage/js/main.js
@@ -0,0 +1,61 @@
+﻿function DarkMode() {
+
+
+    var color = document.getElementsByTagName("h1")[0].style.color ;
+
+
+    if (color == "black") {
+        document.body.style.background = "#1e1e1e"; 
+        document.getElementsByTagName("h1")[0].style.color = "#fff";
+        var pElements = document.getElementsByTagName("p");
+
+        document.getElementById("ModeButton").innerHTML = "LightMode";
+        
+
+        for (var i = 0; i < pElements.length; i++) {
+            pElements[i].style.color = "#fff";
+        }
+        Array.from(document.querySelectorAll('button')).map(function (button) {
+            button.style.backgroundColor = "#373738";
+            button.style.color = "white";
+        })
+        
+    }
+    else {
+        document.body.style.background = "white"; 
+        document.getElementsByTagName("h1")[0].style.color = "black";
+        var pElements = document.getElementsByTagName("p"); 
+
+
+        document.getElementById("ModeButton").innerHTML = "DarkMode";
+
+        for (var i = 0; i < pElements.length; i++) {
+            pElements[i].style.color = "black";
+        }
+        Array.from(document.querySelectorAll('button')).map(function (button)
+        {
+            button.style.backgroundColor = "white";
+            button.style.color = "black";
+        })
+
+    }
+}
+
+
+function ButtonPage1()
+{
+    //if (window.location == "index.html")
+    //{
+    //    window.location.replace("Pages/1ButtonPage.html");
+    //}
+    //else { window.location.replace("index.html");}
+    window.location.replace("Pages/1ButtonPage.html");
+}
+
+function ButtonPage2() {
+    window.location.replace("Pages/2ButtonPage.html");
+}
+
+function ButtonPage3() {
+    window.location.replace("Pages/3ButtonPage.html");
+}
